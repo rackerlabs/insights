b6b5165f0842daac64809996f581d94fbe2171a4
kaustav haldar <hi@kaustav.me>
2015-12-09 18:22:45 -0800

    add some more functions

--- hostinfo/get_hostinfo.py
+++ hostinfo/get_hostinfo.py
@@ -1,3 +1,15 @@
+"""
+The following variables always mean the same things in the following code:
+    @param tenant_id: {string} A rackspace tenant id.
+        e.g. 123456
+    @param auth_token: {string} A valid rackspace auth token for this tenant
+        e.g. ABCDeasfasf2ar32...
+    @param agent_id: {string} Monitoring agent id of the server we want to hit
+        e.g. ABCDe24-123d-3225-af2-34twfetr3
+    @param host_info_types: {string} CSV of the host info types we want
+        e.g. magento || nginx_config,magento,wordpress
+"""
+
 import requests
 import logging
 LOG = logging.getLogger(__name__)
@@ -7,14 +19,7 @@ monitoring_api_url = 'https://monitoring.api.rackspacecloud.com/v1.0/%s'
 
 def get_agent_host_info(tenant_id, auth_token, agent_id, host_info_types):
     """Function to get hostinfo data from a rackspace monitoring agent
-    @param tenant_id: {string} A rackspace tenant id.
-        e.g. 123456
-    @param auth_token: {string} A valid rackspace auth token for this tenant
-        e.g. ABCDeasfasf2ar32...
-    @param agent_id: {string} Monitoring agent id of the server we want to hit
-        e.g. ABCDe24-123d-3225-af2-34twfetr3
-    @param host_info_types: {string} CSV of the host info types we want
-        e.g. magento || nginx_config,magento,wordpress
+
     @return {dict} Contains host information returned with keys = types"""
 
     suffix = ("%s/views/agent_host_info?agentId=%s&include=%s"
@@ -37,13 +42,9 @@ def get_agent_host_info(tenant_id, auth_token, agent_id, host_info_types):
 
 
 def get_agent_host_info_types(tenant_id, auth_token, agent_id):
-    """Function to get hostinfo data from a rackspace monitoring agent
-    @param tenant_id: {string} A rackspace tenant id.
-        e.g. 123456
-    @param auth_token: {string} A valid rackspace auth token for this tenant
-        e.g. ABCDeasfasf2ar32...
-    @param agent_id: {string} Monitoring agent id of the server we want to hit
-        e.g. ABCDe24-123d-3225-af2-34twfetr3
+    """Function to get all available hostinfo types from a rackspace
+    monitoring agent
+
     @return {list} Contains all the supported hostinfos on this agent"""
 
     suffix = ("%s/agents/%s/host_info_types" % (tenant_id, agent_id))
@@ -63,3 +64,46 @@ def get_agent_host_info_types(tenant_id, auth_token, agent_id):
         LOG.debug('Retrieved a list of possible host info types: %s',
                   host_info_types)
         return host_info_types
+
+
+def host_info_to_json(host_info_data):
+    """Given the raw hostinfo data this will rip out the
+    info parts of the hostinfos and discard the timestamps
+    and errors
+    e.g.
+      "x" = {"info": [{validJson}], "timestamp": 12235, "error": "string"}
+    to
+      "x" = [{validJson}]
+    This merely returns validJson, you can get a json string with
+      json.dumps
+    which you can then load back in with
+      json.loads
+    """
+
+    host_info_infos = {}
+    for key in host_info_data:
+        host_info_infos[key] = host_info_data[key]['info']
+    return host_info_infos
+
+
+def get_all_host_info(tenant_id, auth_token, agent_id):
+    """Function to get all hostinfo data from an agent
+    The returned data will contain timestamps, and errors as well
+
+    @return {dict} e.g. {"x" = {"info": [{validJson}], 
+                                "timestamp": 12235,
+                                 "error": "string"}}
+    """
+    host_info_types = get_agent_host_info_types(
+        tenant_id, auth_token, agent_id)
+    host_info_types_string = ",".join(host_info_types)
+    host_info_data_raw = get_agent_host_info(
+        tenant_id, auth_token, agent_id, host_info_types_string)
+    return host_info_data_raw
+
+
+def get_all_host_info_data(tenant_id, auth_token, agent_id):
+    """Function to return hostinfo data stripped of timestamp and error data"""
+    host_info_data_raw = get_all_host_info(tenant_id, auth_token, agent_id)
+    host_info_data_stripped = host_info_to_json(host_info_data_raw)
+    return host_info_data_stripped
